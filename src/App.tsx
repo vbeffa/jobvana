3bddc7e8eec207da7c660b6c16f101473dfcbd62
Vlad Beffa <vlad@jetasap.com>
2025-08-20 15:53:32 -0700

    add tailwind, skills table

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,99 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import supabase from "./utils/supabase";
 
 import "./App.css";
 
-type User = {
+export type User = {
   id: number;
   email: string;
   first_name: string;
   last_name: string;
 };
 
+type Skill = {
+  id: number;
+  name: string;
+  abbreviation: string;
+  skill_type_id: number;
+  notes?: string;
+  parent_skill_id?: number;
+};
+
+type SkillType = {
+  id: number;
+  name: string;
+};
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [users, setUsers] = useState<Array<User>>([]);
+  const [skills, setSkills] = useState<Array<Skill>>([]);
+  const [skillTypes, setSkillTypes] = useState<Array<SkillType>>([]);
 
   useEffect(() => {
-    if (users.length > 0) {
+    if (skills.length > 0) {
       return;
     }
     (async () => {
-      const { data: users } = await supabase.from("users").select();
-      if (users === null) {
+      const { data: skills } = await supabase
+        .from("skills")
+        .select()
+        .order("name");
+      if (skills === null) {
         return;
       }
-      setUsers(users);
+      setSkills(skills);
     })();
-  }, [users.length]);
+  }, [skills.length]);
+
+  useEffect(() => {
+    if (skillTypes.length > 0) {
+      return;
+    }
+    (async () => {
+      const { data: skillTypes } = await supabase.from("skill_types").select();
+      if (skillTypes === null) {
+        return;
+      }
+      setSkillTypes(skillTypes);
+    })();
+  }, [skillTypes.length]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Skills</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="card">
-        <table>
+        <table className="border-1">
           <thead>
             <tr key={0}>
-              <th>Email</th>
-              <th>First Name</th>
-              <th>Last Name</th>
+              <th className="p-1 border">Skill</th>
+              <th className="p-1 border">Abbreviation</th>
+              <th className="p-1 border">Skill Type</th>
+              <th className="p-1 border">Notes</th>
+              <th className="p-1 border">Parent Skill</th>
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => {
+            {skills.map((skill) => {
               return (
-                <tr key={user.id}>
-                  <td>{user.email}</td>
-                  <td>{user.first_name}</td>
-                  <td>{user.last_name}</td>
+                <tr key={skill.id}>
+                  <td className="p-1 border text-left">{skill.name}</td>
+                  <td className="p-1 border text-left">{skill.abbreviation}</td>
+                  <td className="p-1 border text-left">
+                    {
+                      skillTypes.find(
+                        (skillType) => skillType.id === skill.skill_type_id
+                      )?.name
+                    }
+                  </td>
+                  <td className="p-1 border text-left">{skill.notes}</td>
+                  <td className="p-1 border text-left">
+                    {skills.find((s) => s.id === skill.parent_skill_id)?.name}
+                  </td>
                 </tr>
               );
             })}
           </tbody>
         </table>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
