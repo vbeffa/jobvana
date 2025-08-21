63c00dd4fc716d412e6abc3bf825e50aa67855d1
Vlad Beffa <vlad@jetasap.com>
2025-08-21 13:53:26 -0700

    fix home link

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function Header() {
   return (
     <div className="bg-amber-300 w-full h-12 text-left pl-2 pt-2 mb-4 top-0 left-0 sticky">
-      <a href="/">Home</a>
+      <a href="/jobvana">Home</a>
     </div>
   );
 }
