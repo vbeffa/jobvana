f64ad6cef21ed7fff62672e0eede5dacf41a9bd3
Vlad Beffa <vlad@jetasap.com>
2025-08-21 13:56:23 -0700

    links in header

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 function Header() {
   return (
     <div className="bg-amber-300 w-full h-12 text-left pl-2 pt-2 mb-4 top-0 left-0 sticky">
-      <a href="/jobvana">Home</a>
+      <a href="/jobvana">Home</a> <a href="/jobvana/jobs/">Jobs</a>{" "}
+      <a href="/jobvana/skills/">Skills</a>
     </div>
   );
 }
