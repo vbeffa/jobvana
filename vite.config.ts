71d6fc62f97a389f611e720e8909053ca528239c
Vlad Beffa <vlad@jetasap.com>
2025-08-23 16:17:13 -0700

    clean up old dirs

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "jobvana",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        skills: resolve(__dirname, "skills/index.html"),
-        jobs: resolve(__dirname, "jobs/index.html"),
-        companies: resolve(__dirname, "companies/index.html")
-      }
-    }
-  }
+  base: "jobvana"
 });
