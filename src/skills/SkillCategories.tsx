1870f01a4e0656ad709fed03476a5a364ba23619
Vlad Beffa <vlad@jetasap.com>
2025-08-25 15:46:19 -0700

    skill categories filtering

--- src/skills/SkillCategories.tsx
+++ src/skills/SkillCategories.tsx
@@ -3,14 +3,15 @@ import useSkills from "../hooks/useSkills";
 import Loading from "../Loading";
 import SkillCategoryLink from "./SkillCategoryLink";
 import SkillsList from "./SkillsList";
+import Filter from "../Filter";
 
-type SortCol = "skill_category" | "skills";
+type SortCol = "skill_category";
 type SortDir = "up" | "down";
 
 const SkillCategories = () => {
   const [sortCol, setSortCol] = useState<SortCol>("skill_category");
   const [sortDir, setSortDir] = useState<SortDir>("up");
-  const [skillCategoriesFilter, setSkillCategoriesFilter] = useState<string>();
+  const [categoryFilter, setCategoryFilter] = useState<string>();
 
   const { skillCategories, findSkills } = useSkills();
 
@@ -18,10 +19,10 @@ const SkillCategories = () => {
     return skillCategories
       ?.filter((skillCategory) => {
         let pass = true;
-        if (skillCategoriesFilter) {
+        if (categoryFilter) {
           pass = skillCategory.name
             .toLocaleLowerCase()
-            .includes(skillCategoriesFilter.toLocaleLowerCase());
+            .includes(categoryFilter.toLocaleLowerCase());
         }
         return pass;
       })
@@ -30,7 +31,7 @@ const SkillCategories = () => {
           ? skillCategory1.name.localeCompare(skillCategory2.name)
           : skillCategory2.name.localeCompare(skillCategory1.name);
       });
-  }, [skillCategories, skillCategoriesFilter, sortDir]);
+  }, [skillCategories, categoryFilter, sortDir]);
 
   const setSort = (col: SortCol) => {
     const newSortCol = col;
@@ -51,17 +52,21 @@ const SkillCategories = () => {
         <table className="w-full">
           <thead>
             <tr>
-              <td className="text-left pb-2 w-[50%]">
-                <input
-                  type="text"
-                  className="border pl-1"
-                  placeholder="Filter by skill category"
-                  onChange={(e) => {
-                    setSkillCategoriesFilter(e.target.value);
-                  }}
-                />
+              <td className="text-left">
+                <div className="w-[50%]">
+                  <Filter
+                    id="category_filter"
+                    placeholder="Filter by skill category"
+                    value={categoryFilter}
+                    onChange={setCategoryFilter}
+                    onClear={() => setCategoryFilter("")}
+                  />
+                </div>
               </td>
               <td></td>
+            </tr>
+            <tr>
+              <td className="h-1" colSpan={3} />
             </tr>
             <tr>
               <th
@@ -71,12 +76,7 @@ const SkillCategories = () => {
                 Category{" "}
                 {sortCol === "skill_category" && (sortDir === "up" ? "↑" : "↓")}
               </th>
-              <th
-                className="p-1 border cursor-pointer"
-                onClick={() => setSort("skills")}
-              >
-                Skills {sortCol === "skills" && (sortDir === "up" ? "↑" : "↓")}
-              </th>
+              <th className="p-1 border">Skills</th>
             </tr>
           </thead>
           <tbody>
