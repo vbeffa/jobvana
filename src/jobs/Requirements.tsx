40b54a185e4ebf56752e03f9bdf05521e66f034e
Vlad Beffa <vlad@jetasap.com>
2025-09-06 16:20:11 -0700

    role level bar

--- src/jobs/Requirements.tsx
+++ src/jobs/Requirements.tsx
@@ -1,7 +1,6 @@
 import { type JobRole } from '../hooks/useJobs';
 import useRoles from '../hooks/useRoles';
 import RoleLink from '../roles/RoleLink';
-import { roleLevel } from '../roles/utils';
 
 export type RequirementsProps = {
   requirements: Array<Pick<JobRole, 'role_id' | 'percent' | 'role_level'>>;
@@ -9,6 +8,41 @@ export type RequirementsProps = {
 
 const Requirements = ({ requirements }: RequirementsProps) => {
   const { roles } = useRoles();
+
+  const levelBar = (level: number) => {
+    switch (level) {
+      case 0:
+        return (
+          <div className="border-[0.5px] inline-block w-10 h-2">
+            <div className="w-2 h-2 bg-blue-500"></div>
+          </div>
+        );
+      case 1:
+        return (
+          <div className="border-[0.5px] inline-block w-10 h-2">
+            <div className="w-4 h-2 bg-blue-500"></div>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="border-[0.5px] inline-block w-10 h-2">
+            <div className="w-6 h-2 bg-blue-500"></div>
+          </div>
+        );
+      case 3:
+        return (
+          <div className="border-[0.5px] inline-block w-10 h-2">
+            <div className="w-8 h-2 bg-blue-500"></div>
+          </div>
+        );
+      case 4:
+        return (
+          <div className="border-[0.5px] inline-block w-10 h-2">
+            <div className="w-10 h-2 bg-blue-500"></div>
+          </div>
+        );
+    }
+  };
 
   return (
     <ul>
@@ -18,7 +52,7 @@ const Requirements = ({ requirements }: RequirementsProps) => {
           role && (
             <li key={role.id}>
               <RoleLink {...role} /> ({requirement.percent}%,{' '}
-              {roleLevel(requirement.role_level)})
+              {levelBar(requirement.role_level)})
             </li>
           )
         );
