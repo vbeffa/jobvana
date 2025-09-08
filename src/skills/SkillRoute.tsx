c7af7a0078015c4220dae3b7a2b25b1219faf0ae
Vlad Beffa <vlad@jetasap.com>
2025-09-08 10:06:15 -0700

    fix SkillRoute

--- src/skills/SkillRoute.tsx
+++ src/skills/SkillRoute.tsx
@@ -1,26 +1,10 @@
-import Error from '../Error';
-import useSkill from '../hooks/useSkill';
-import LoadingModal from '../LoadingModal';
 import { Route } from '../routes/jobvana.skill_categories.$id.skills.$skill_id.index';
 import SkillDetails from './SkillDetails';
 
 const SkillRoute = () => {
   const { skillId } = Route.useLoaderData();
-  const { skill, error, isPending, isPlaceholderData } = useSkill(skillId);
 
-  if (error) {
-    return <Error error={error} />;
-  }
-
-  if (isPending) {
-    return <LoadingModal />;
-  }
-
-  if (!skill) {
-    return null;
-  }
-
-  return <SkillDetails skill={skill} isPlaceholderData={isPlaceholderData} />;
+  return <SkillDetails id={skillId} />;
 };
 
 export default SkillRoute;
