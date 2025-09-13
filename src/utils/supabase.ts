36c9bad0de2655395078925d84b1fe37c0af572a
Vlad Beffa <vlad@jetasap.com>
2025-09-13 10:26:29 -0700

    remove commented channel code

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -5,12 +5,5 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
-// const channelA = supabase
-//   .channel("skills-changes")
-//   .on("postgres_changes", { event: "*", schema: "public" }, (payload) =>
-//     console.log(payload)
-//   )
-//   .subscribe();
-// console.log(channelA);
 
 export default supabase;
