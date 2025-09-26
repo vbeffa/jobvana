b59e78ede80aa4856f4092c1c0a5481327529349
Vlad Beffa <vlad@jetasap.com>
2025-09-25 18:40:46 -0700

    remove ExpandCollapse

--- src/jobs/company/MyJobRoles.tsx
+++ src/jobs/company/MyJobRoles.tsx
@@ -8,7 +8,6 @@ import supabase from '../../utils/supabase';
 import JobRoles from '../JobRoles';
 import LevelSelect from '../LevelSelect';
 import RoleSelect from '../RoleSelect';
-import ExpandCollapse from './ExpandCollapse';
 import type { Edit } from './MyJobs';
 import PercentInput from './PercentInput';
 import type { Job } from './useJobsForCompany';
@@ -28,7 +27,6 @@ const MyJobRoles = ({
   edit,
   setEdit
 }: MyJobRolesProps) => {
-  const [expanded, setExpanded] = useState(true);
   const [editJobRoles, setEditJobRoles] = useState<Array<JobRole>>(
     job.job_roles
   );
@@ -118,157 +116,150 @@ const MyJobRoles = ({
   return (
     <>
       {error && <Error error={error} />}
-      <ExpandCollapse
-        title="Roles"
-        expanded={expanded}
-        setExpanded={setExpanded}
-      />
-      {expanded && (
-        <div className="grid grid-cols-[15%_75%] gap-y-2 relative">
-          <EditDeleteIcons
-            isEditing={isEditing}
-            setIsEditing={(isEditing) => {
-              if (isEditing) {
-                setError(undefined);
-                setEdit({ jobId: job.id, section: 'roles' });
-                setEditJobRoles(job.job_roles);
-              }
-              setIsEditing(isEditing);
-            }}
-            disabled={isEditing && (!isDirty || !isValid || isSubmitting)}
-            onSave={doUpdate}
-          />
-          {!isEditing && (
-            <>
-              <div>Roles:</div>
-              <div>
-                <JobRoles jobRoles={job.job_roles} />
-              </div>
-            </>
-          )}
-          {isEditing && (
-            <>
-              <label htmlFor="role_0" className="content-center">
-                Roles:
-              </label>
-              {editJobRoles.map((jobRole, idx) => (
-                <div
-                  key={`role_${idx}_div`}
-                  className="col-start-2 flex flex-row gap-2"
-                >
-                  <RoleSelect
-                    id={`role_${idx}`}
-                    roleId={jobRole.role_id}
-                    showAny={false}
-                    onChange={(roleId) => {
-                      setEditJobRoles((jobRoles) => {
-                        const updatedJobRoles = _.cloneDeep(jobRoles);
-                        updatedJobRoles[idx].role_id = roleId;
-                        return updatedJobRoles;
-                      });
-                    }}
-                  />
-                  <div className="relative flex flex-row gap-0.5">
-                    <PercentInput
-                      idx={idx}
-                      value={jobRole.percent}
-                      onChange={(percent) => {
-                        setEditJobRoles((roles) => {
-                          const updatedRoles = _.cloneDeep(roles);
-                          updatedRoles[idx].percent = percent;
-                          return updatedRoles;
-                        });
-                      }}
-                    />
-                    <div
-                      className={`absolute ${
-                        jobRole.percent >= 100
-                          ? 'right-6'
-                          : jobRole.percent >= 10
-                            ? 'right-8'
-                            : 'right-10'
-                      } top-1 -z-10`}
-                    >
-                      %
-                    </div>
-                  </div>
-                  <LevelSelect
-                    id={`level_${idx}`}
-                    value={jobRole.role_level}
-                    onChange={(level) => {
-                      setEditJobRoles((roles) => {
-                        const updatedRoles = _.cloneDeep(roles);
-                        updatedRoles[idx].role_level = level;
-                        return updatedRoles;
-                      });
-                    }}
-                  />
-                  <div
-                    className="content-center text-sm text-gray-400 cursor-pointer"
-                    onClick={() => {
-                      setEditJobRoles((roles) => {
-                        const updatedRoles = _.cloneDeep(roles);
-                        updatedRoles.splice(idx, 1);
-                        return updatedRoles;
-                      });
-                    }}
-                  >
-                    <FaTrash />
-                  </div>
-                  {(editJobRoles[idx].percent < 1 ||
-                    editJobRoles[idx].percent > 100) && (
-                    <div className="text-sm text-red-500 flex flex-row gap-1">
-                      <div className="content-center">
-                        <FaTriangleExclamation />
-                      </div>
-                      <div className="content-center">% out of range</div>
-                    </div>
-                  )}
-                </div>
-              ))}
+      <div className="grid grid-cols-[15%_75%] gap-y-2 relative">
+        <EditDeleteIcons
+          isEditing={isEditing}
+          setIsEditing={(isEditing) => {
+            if (isEditing) {
+              setError(undefined);
+              setEdit({ jobId: job.id, section: 'roles' });
+              setEditJobRoles(job.job_roles);
+            }
+            setIsEditing(isEditing);
+          }}
+          disabled={isEditing && (!isDirty || !isValid || isSubmitting)}
+          onSave={doUpdate}
+        />
+        {!isEditing && (
+          <>
+            <div>Roles:</div>
+            <div>
+              <JobRoles jobRoles={job.job_roles} />
+            </div>
+          </>
+        )}
+        {isEditing && (
+          <>
+            <label htmlFor="role_0" className="content-center">
+              Roles:
+            </label>
+            {editJobRoles.map((jobRole, idx) => (
               <div
-                className={`relative col-start-2 ${percentTotalValid ? 'pb-5' : 'pb-0'}`}
+                key={`role_${idx}_div`}
+                className="col-start-2 flex flex-row gap-2"
               >
-                {!percentTotalValid && (
-                  <div className="text-sm text-red-500 flex flex-row gap-1 pl-50">
-                    <div className="content-center">
-                      <FaTriangleExclamation />
-                    </div>
-                    <div>{percentTotal}%</div>
-                  </div>
-                )}
-                {duplicateRole && (
-                  <div className="absolute left-0 top-0 text-sm text-red-500 flex flex-row gap-1">
-                    <div className="content-center">
-                      <FaTriangleExclamation />
-                    </div>
-                    <div>Duplicate role</div>
-                  </div>
-                )}
-                {!duplicateRole && (
-                  <div
-                    className="absolute left-0 top-0 text-gray-400 cursor-pointer"
-                    onClick={() => {
+                <RoleSelect
+                  id={`role_${idx}`}
+                  roleId={jobRole.role_id}
+                  showAny={false}
+                  onChange={(roleId) => {
+                    setEditJobRoles((jobRoles) => {
+                      const updatedJobRoles = _.cloneDeep(jobRoles);
+                      updatedJobRoles[idx].role_id = roleId;
+                      return updatedJobRoles;
+                    });
+                  }}
+                />
+                <div className="relative flex flex-row gap-0.5">
+                  <PercentInput
+                    idx={idx}
+                    value={jobRole.percent}
+                    onChange={(percent) => {
                       setEditJobRoles((roles) => {
                         const updatedRoles = _.cloneDeep(roles);
-                        updatedRoles.push({
-                          job_id: job.id,
-                          role_id: 1,
-                          percent: 100,
-                          role_level: 2
-                        });
+                        updatedRoles[idx].percent = percent;
                         return updatedRoles;
                       });
                     }}
+                  />
+                  <div
+                    className={`absolute ${
+                      jobRole.percent >= 100
+                        ? 'right-6'
+                        : jobRole.percent >= 10
+                          ? 'right-8'
+                          : 'right-10'
+                    } top-1 -z-10`}
                   >
-                    <FaPlus />
+                    %
+                  </div>
+                </div>
+                <LevelSelect
+                  id={`level_${idx}`}
+                  value={jobRole.role_level}
+                  onChange={(level) => {
+                    setEditJobRoles((roles) => {
+                      const updatedRoles = _.cloneDeep(roles);
+                      updatedRoles[idx].role_level = level;
+                      return updatedRoles;
+                    });
+                  }}
+                />
+                <div
+                  className="content-center text-sm text-gray-400 cursor-pointer"
+                  onClick={() => {
+                    setEditJobRoles((roles) => {
+                      const updatedRoles = _.cloneDeep(roles);
+                      updatedRoles.splice(idx, 1);
+                      return updatedRoles;
+                    });
+                  }}
+                >
+                  <FaTrash />
+                </div>
+                {(editJobRoles[idx].percent < 1 ||
+                  editJobRoles[idx].percent > 100) && (
+                  <div className="text-sm text-red-500 flex flex-row gap-1">
+                    <div className="content-center">
+                      <FaTriangleExclamation />
+                    </div>
+                    <div className="content-center">% out of range</div>
                   </div>
                 )}
               </div>
-            </>
-          )}
-        </div>
-      )}
+            ))}
+            <div
+              className={`relative col-start-2 ${percentTotalValid ? 'pb-5' : 'pb-0'}`}
+            >
+              {!percentTotalValid && (
+                <div className="text-sm text-red-500 flex flex-row gap-1 pl-50">
+                  <div className="content-center">
+                    <FaTriangleExclamation />
+                  </div>
+                  <div>{percentTotal}%</div>
+                </div>
+              )}
+              {duplicateRole && (
+                <div className="absolute left-0 top-0 text-sm text-red-500 flex flex-row gap-1">
+                  <div className="content-center">
+                    <FaTriangleExclamation />
+                  </div>
+                  <div>Duplicate role</div>
+                </div>
+              )}
+              {!duplicateRole && (
+                <div
+                  className="absolute left-0 top-0 text-gray-400 cursor-pointer"
+                  onClick={() => {
+                    setEditJobRoles((roles) => {
+                      const updatedRoles = _.cloneDeep(roles);
+                      updatedRoles.push({
+                        job_id: job.id,
+                        role_id: 1,
+                        percent: 100,
+                        role_level: 2
+                      });
+                      return updatedRoles;
+                    });
+                  }}
+                >
+                  <FaPlus />
+                </div>
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 };
