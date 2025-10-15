d32cb625bed8b99d8e776b9ed1d175619f2415d0
Vlad Beffa <vlad@jetasap.com>
2025-10-15 15:52:05 -0700

    tabs for status select (commented out)

--- src/jobs/company/StatusSelect.tsx
+++ src/jobs/company/StatusSelect.tsx
@@ -3,17 +3,39 @@ import type { JobStatus } from '../../types';
 
 const StatusSelect = ({
   status,
+  disabled,
   onChange
 }: {
-  status?: JobStatus | 'all';
+  status: JobStatus | 'all';
+  disabled?: boolean;
   onChange: (status: JobStatus | 'all') => void;
 }) => {
+  // const link = (linkStatus: JobStatus | 'all') => (
+  //   <div
+  //     className={`${linkStatus === status ? 'border-b-2 border-blue-500' : ''} flex flex-row items-center gap-1 cursor-pointer`}
+  //     onClick={() => onChange(linkStatus)}
+  //   >
+  //     <div className="text-sm">
+  //       <FaDraftingCompass />
+  //     </div>
+  //     {capitalize(linkStatus)}
+  //   </div>
+  // );
+
   return (
     <div>
+      {/* <div className="flex flex-row gap-2">
+      {link('all')}
+      {link('draft')}
+      {link('open')}
+      {link('closed')}
+      {link('filled')} */}
+
       <Select
         id="status"
         value={status}
         width="w-34"
+        disabled={disabled}
         onChange={(e) => {
           const status = e.target.value as JobStatus | 'all';
           onChange(status);
