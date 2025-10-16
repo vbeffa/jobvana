bd5f66ad737146aaa88d7d6605b8ad0f34af7dca
Vlad Beffa <vlad@jetasap.com>
2025-10-16 09:37:20 -0700

    add root error component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,26 @@
-import { createRootRouteWithContext } from '@tanstack/react-router';
+import { createRootRouteWithContext, Link } from '@tanstack/react-router';
+import { FaFrown } from 'react-icons/fa';
 import type { JobvanaContextProps } from '../Context';
 import Root from '../Root';
 
 export const Route = createRootRouteWithContext<JobvanaContextProps>()({
-  component: Root
+  component: Root,
+  errorComponent: () => {
+    return (
+      <div className="flex justify-center mt-[25dvh]">
+        <div className="border-[0.5px] border-blue-400 rounded-lg bg-blue-200 p-4">
+          <div className="flex flex-row gap-1 items-center">
+            <FaFrown className="text-blue-400" /> An unexpected error occurred.{' '}
+            <FaFrown className="text-blue-400" />
+          </div>
+          <div className="flex justify-center">
+            <Link to=".">Return home.</Link>
+          </div>
+          <div className="flex justify-center">
+            <a href="mailto:support@jobvana.io">Contact us.</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 });
