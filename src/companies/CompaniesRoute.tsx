1964635a6db31d13513c12b9b3fd90be751dec0b
Vlad Beffa <6473744+vbeffa@users.noreply.github.com>
2026-09-13 15:13:23 -0700

    Use declarative company redirect

--- src/companies/CompaniesRoute.tsx
+++ src/companies/CompaniesRoute.tsx
@@ -1,19 +1,16 @@
-import { useNavigate } from '@tanstack/react-router';
-import { useContext, useEffect } from 'react';
+import { Navigate } from '@tanstack/react-router';
+import { useContext } from 'react';
 import { JobvanaContext } from '../Context';
 import Companies from './job_seeker/Companies';
 
 const CompaniesRoute = () => {
-  const navigate = useNavigate();
   const { userType } = useContext(JobvanaContext);
 
-  useEffect(() => {
-    if (userType === 'company') {
-      navigate({ to: '/jobvana' });
-    }
-  }, [navigate, userType]);
-
-  return userType === 'job_seeker' ? <Companies /> : null;
+  return userType === 'company' ? (
+    <Navigate to="/jobvana" />
+  ) : userType === 'job_seeker' ? (
+    <Companies />
+  ) : null;
 };
 
 export default CompaniesRoute;
