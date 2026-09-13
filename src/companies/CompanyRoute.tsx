ac010ede328273a0c0b21e34a395a1f746519a45
Vlad Beffa <6473744+vbeffa@users.noreply.github.com>
2026-09-13 14:25:27 -0700

    Use trusted user type for company views

--- src/companies/CompanyRoute.tsx
+++ src/companies/CompanyRoute.tsx
@@ -1,14 +1,13 @@
 import { useContext } from 'react';
-import { getUserType } from '../auth/utils';
 import ResourceContainer from '../containers/ResourceContainer';
-import { CompanyContext } from '../Context';
+import { CompanyContext, JobvanaContext } from '../Context';
 import { Route } from '../routes/jobvana.companies.$id';
 import MyCompany from './company/MyCompany';
 import CompanyDetails from './job_seeker/CompanyDetails';
 
 const CompanyRoute = () => {
   const { id } = Route.useLoaderData();
-  const userType = getUserType();
+  const { userType } = useContext(JobvanaContext);
   const { company } = useContext(CompanyContext);
 
   // companies can only view their own companies
