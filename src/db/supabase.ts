3561cedcc446141d319058a12a5ed349d44daf37
Vlad Beffa <vbeffa@mac.com>
2026-09-15 18:42:55 -0700

    add retry: false to createClient to preserve prior semantics

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -4,6 +4,10 @@ import type { Database } from './types';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+  db: {
+    retry: false
+  }
+});
 
 export default supabase;
