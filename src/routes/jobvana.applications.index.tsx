d54b1fe9c865c5d26a64d47e5e1989bc3649395d
Vlad Beffa <6473744+vbeffa@users.noreply.github.com>
2026-09-18 21:00:05 +0000

    Use dedicated applications route component

--- src/routes/jobvana.applications.index.tsx
+++ src/routes/jobvana.applications.index.tsx
@@ -1,19 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { useContext } from 'react';
-import CompanyApplications from '../applications/company/Applications';
-import JobSeekerApplications from '../applications/job_seeker/Applications';
-import { CompanyContext, JobSeekerContext, JobvanaContext } from '../Context';
+import ApplicationsRoute from '../applications/ApplicationsRoute';
 
 export const Route = createFileRoute('/jobvana/applications/')({
-  component: Switcher
+  component: ApplicationsRoute
 });
-
-function Switcher() {
-  const { userType } = useContext(JobvanaContext);
-  const { company } = useContext(CompanyContext);
-  const { jobSeeker } = useContext(JobSeekerContext);
-
-  return userType === 'company'
-    ? company && <CompanyApplications companyId={company.id} />
-    : jobSeeker && <JobSeekerApplications jobSeekerId={jobSeeker.id} />;
-}
