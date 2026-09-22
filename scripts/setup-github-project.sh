0b9ac497808c8bb335b0d1a113b761a46821bcab
Vlad Beffa <6473744+vbeffa@users.noreply.github.com>
2026-09-22 15:34:16 -0700

    Fix GitHub Project setup script content

--- scripts/setup-github-project.sh
+++ scripts/setup-github-project.sh
@@ -93,40 +93,199 @@ TODO_ID="$(
 )"
 IN_PROGRESS_ID="$(
   gh project field-list "$PROJECT_NUMBER" --owner "$OWNER" --format json \
-   --jq '.fields[] | select(.name=="Status") | .options[] | select(.name=="In Progress") | .id'
+    --jq '.fields[] | select(.name=="Status") | .options[] | select(.name=="In Progress") | .id'
 )"
 DONE_ID="$(
   gh project field-list "$PROJECT_NUMBER" --owner "$OWNER" --format json \
     --jq '.fields[] | select(.name=="Status") | .options[] | select(.name=="Done") | .id'
 )"
 
-b| ÖÈ^ÕUT×ÑQSÒQ^Ñ×ÒQ^SÔÑÔTÔ×ÒQ^ÓWÒQWNÈ[XÚÈ\ÜÛÝ[ÝY[YHHY][Ý]\ÈY[ÛÜ[ÛÈ^]BBÚ\HÜ\[Y]Y\OH]]][ÛÂ\]TÚXÝY[
-[]ÂY[YÕUT×ÑQSÒQÚ[ÛTÙ[XÝÜ[ÛÎÂÚYÑ×ÒQ[YNXÚÛÙ×ÛÛÜÔVK\ØÜ\[ÛÝY]XYHÈÝ\BÈ[YNXYWÛÛÜQK\ØÜ\[ÛXYHÈÛÜÈÛBÚYSÔÑÔTÔ×ÒQ[YN[ÙÜ\Ü×ÛÛÜQSÕË\ØÜ\[ÛÝ\[HZ[ÈÛÜÙYÛBÈ[YN[]Y]×ÛÛÜTK\ØÜ\[Û]ØZ][È]Y]ÈÜY\ÙWBÚYÓWÒQ[YNÛWÛÛÜÔQS\ØÜ\[ÛÛÛ\]YBBJHÂÚXÝY[ÂÛÚXÝÚ[ÛTÙ[XÝY[Â[YBÜ[ÛÈÈY[YHBBBBHÙ]Û[XÚÈ[\Ü[ÈÜ[\ÜÝY\ÈÛH	SÔTËÚ\ÜÝYH\ÝTSÔTÈK\Ý]HÜ[K[[Z]LKZÛÛ\KZH	Ë×K\	ÈÚ[HXY\\ÈÂÖÈ[\WHÛÛ[YBÚÚXÝ][KXYÒPÕÓSPTK[ÝÛ\ÕÓTK]\\Ù]Û[XÚÈYY	\ÛBÙ]Ú\ÜÝYWÙY[
+if [[ -z "$STATUS_FIELD_ID" || -z "$TODO_ID" || -z "$IN_PROGRESS_ID" || -z "$DONE_ID" ]]; then
+  echo "error: could not identify the default Status field/options" >&2
+  exit 1
+fi
 
-HÂØØ[\ÜÝYWÛ[X\HHØØ[Y[HØØ[[YOHÈØØ[Ý]BÝ]OH
-Ú\ÜÝYHY]È\ÜÝYWÛ[X\TSÔTÈKZÛÛÝ]HKZH	ËÝ]IÊHYÖÈÝ]HOHÔSWNÈ[XÚÈÚÚ\[ÈÛÜÙY\ÜÝYHÉ\ÜÝYWÛ[X\
-	Y[I[YJH]\BÚÚXÝ][KYY]ÒPÕÓSPTK[ÝÛ\ÕÓTK]\ÎËÙÚ]XÛÛKÉSÔTËÚ\ÜÝY\ËÉ\ÜÝYWÛ[X\KYY[Y[K][YH[YHÙ]Û[BÙ]ÛX[J
-HÂØØ[Y[HHØØ[[YOHÚYØØ[Ü[ÈÂÙ]Ú\ÜÝYWÙY[Y[[YHÛBBXÚÈ\ÜÚYÛ[È[Ü]Y\ËÙ]ÛX[H[Ü]HYÚÔSÔUWÒQÒÐ_HÙ]ÛX[H[Ü]HYY][HÔSÔUWÓQQUSVÐ_HÙ]ÛX[H[Ü]HÝÈÔSÔUWÓÕÖÐ_HXÚÈ\ÜÚYÛ[È\X\ËÙ]ÛX[H\XHÙXÝ\]HÐTPWÔÑPÕTUVÐ_HÙ]ÛX[H\XH]HÐTPWÑUVÐ_HÙ]ÛX[H\XHXÚÙ[ÐTPWÐPÒÑSÐ_HÙ]ÛX[H\XHÛ[ÐTPWÑÓSÐ_HÙ]ÛX[H\XH[\ÝXÝ\HÐTPWÒSTÕPÕTVÐ_HY
+gh api graphql -f query="
+mutation {
+  updateProjectV2Field(input: {
+    fieldId: \"$STATUS_FIELD_ID\"
+    singleSelectOptions: [
+      {id: \"$TODO_ID\",        name: \"Backlog\",     color: GRAY,   description: \"Not yet ready to start\"}
+      {                         name: \"Ready\",       color: BLUE,   description: \"Ready to work on\"}
+      {id: \"$IN_PROGRESS_ID\", name: \"In progress\", color: YELLOW, description: \"Currently being worked on\"}
+      {                         name: \"In review\",   color: PURPLE, description: \"Awaiting review or merge\"}
+      {id: \"$DONE_ID\",        name: \"Done\",        color: GREEN,  description: \"Completed\"}
+    ]
+  }) {
+    projectV2Field {
+      ... on ProjectV2SingleSelectField {
+        name
+        options { id name }
+      }
+    }
+  }
+}" >/dev/null
 
-	ÈÐTPWÑÐÕSQSUSÓÐ_JJNÈ[Ù]ÛX[H\XHØÝ[Y[][ÛÐTPWÑÐÕSQSUSÓÐ_HBÈY\ÜÝYHÍHÝ[\È]È[\[Y[][Û
-ÍÊHÜ[YXÝ][HØ\YÖÈ
-Ú\ÜÝYHY]ÈHTSÔTÈKZÛÛÝ]HKZH	ËÝ]IÊHOHÔSWH	ÖÈ
-ÚY]ÈÈTSÔTÈKZÛÛÝ]HKZH	ËÝ]IÈÙ]Û[YJHOHÔSWNÈ[XÚÈX\Ú[È\ÜÝYHÍH[]Y]ËÙ]Ú\ÜÝYWÙY[HÝ]\È[]Y]ÈBXÚÈÛÛYÝ\[ÈHY][XHY]ËUWÑQSÒQH
-ÚÚXÝY[[\ÝÒPÕÓSPTK[ÝÛ\ÕÓTKYÜX]ÛÛKZH	ËY[Ö×HÙ[XÝ
-[YOOH]HHY	ÂHSÔUWÑQSÒQH
-ÚÚXÝY[[\ÝÒPÕÓSPTK[ÝÛ\ÕÓTKYÜX]ÛÛKZH	ËY[Ö×HÙ[XÝ
-[YOOH[Ü]HHY	ÂHTPWÑQSÒQH
-ÚÚXÝY[[\ÝÒPÕÓSPTK[ÝÛ\ÕÓTKYÜX]ÛÛKZH	ËY[Ö×HÙ[XÝ
-[YOOH\XHHY	ÂHPÒÓÑ×ÕQU×ÒQH
-Ú\HÜ\[Y]Y\OIÜ]Y\J	ÙÚ[Ý[ÈK	[X\[JHÂ\Ù\ÙÚ[	ÙÚ[HÂÚXÝ[X\	[X\HÂY]ÜÊ\Ý
-HÈÙ\ÈÈY[YH^[Ý]HBBBIÈYÙÚ[HÕÓTQ[X\HÒPÕÓSPTKZH	Ë]K\Ù\ÚXÝY]ÜËÙ\Ö×HÙ[XÝ
-^[Ý]OHPWÓVSÕUHY	ÈXY[BHYÖÈ^PÒÓÑ×ÕQU×ÒQWNÈ[XÚÈ\ÜÛÝ[ÝØØ]HHY][XHY]È^]BBÚ\HÜ\[Y]Y\OH]]][ÛÂ\]TÚXÝY]Ê[]ÂY]ÒYPÒÓÑ×ÕQU×ÒQ[YNXÚÛÙ×ÛÛYÝ\][ÛÂ\ÚXQY[YÎÂUWÑQSÒQÕUT×ÑQSÒQSÔUWÑQSÒQTPWÑQSÒQBBJHÂÚXÝY]ÈÈY[YH^[Ý]BBHÙ]Û[XÚÈÜX][ÈÛÜÙÝÈØ\UWÔTÕÒQH
-Ú\HRQÚ]XP\KU\Ú[Û	TWÕTÒSÓÝ\Ù\ËÉÕÓTÜÚXÝÕÉÒPÕÓSPTÙY[ÈKZH	Ë×HÙ[XÝ
-[YOOH]HHY	ÂHÕUT×ÔTÕÒQH
-Ú\HRQÚ]XP\KU\Ú[Û	TWÕTÒSÓÝ\Ù\ËÉÕÓTÜÚXÝÕÉÒPÕÓSPTÙY[ÈKZH	Ë×HÙ[XÝ
-[YOOHÝ]\ÈHY	ÂHSÔUWÔTÕÒQH
-Ú\HRQÚ]XP\KU\Ú[Û	TWÕTÒSÓÝ\Ù\ËÉÕÓTÜÚXÝÕÉÒPÕÓSPTÙY[ÈKZH	Ë×HÙ[XÝ
-[YOOH[Ü]HHY	ÂHTPWÔTÕÒQH
-Ú\HRQÚ]XP\KU\Ú[Û	TWÕTÒSÓÝ\Ù\ËÉÕÓTÜÚXÝÕÉÒPÕÓSPTÙY[ÈKZH	Ë×HÙ[XÝ
-[YOOH\XHHY	ÂHÓÔÑÕ×ÕTH
-Ú\HVÔÕRXØÙ\\XØ][ÛÝÚ]XÚÛÛRQÚ]XP\KU\Ú[Û	TWÕTÒSÓÝ\Ù\ËÉÕÓTÜÚXÝÕÉÒPÕÓSPTÝY]ÜÈY[YOHÛÜÙÝÈY^[Ý]HØ\Y[\H\ÎÜ[Q\ÚXWÙY[Ö×OIUWÔTÕÒQQ\ÚXWÙY[Ö×OISÔUWÔTÕÒQQ\ÚXWÙY[Ö×OITPWÔTÕÒQQ\XØ[ÙÜÝ\ØV×OIÕUT×ÔTÕÒQKZH	Ë[Ý\	ÂHXÚÂXÚÈÚXÝÙ]\ÛÛ\]KXÚÈÚXÝ	ÒPÕÕTXÚÈÛÜÙÝÎ	ÓÔÑÕ×ÕTXÚÂXÚÈÛHX[X[Ý\[XZ[ÎXÚÈÚXÝOOÛÜÙÝÜÈO]]ËXYÈÚXÝOY]XÚÈ\ÜÚ]ÜN	SÔTÈXÚÈ[\\Î\ÜÝYH\ÎÜ[XÚÈ[ÚÛÜÙNØ]H[\ÛÛÜÙÝÈXÚÂXÚÈÝ\[ÚXÝ][\ÎÚÚXÝ][K[\ÝÒPÕÓSPTK[ÝÛ\ÕÓTK[[Z]KYY[Ý]\ÈKYY[[Ü]HKYY[\XB
+echo "Importing open issues from $FULL_REPO..."
+gh issue list -R "$FULL_REPO" --state open --limit 500 --json url --jq '.[].url' |
+while read -r url; do
+  [[ -n "$url" ]] || continue
+  gh project item-add "$PROJECT_NUMBER" --owner "$OWNER" --url "$url" >/dev/null
+  echo "  added $url"
+done
+
+set_issue_field() {
+  local issue_number="$1"
+  local field="$2"
+  local value="$3"
+  local state
+
+  state="$(gh issue view "$issue_number" -R "$FULL_REPO" --json state --jq '.state')"
+  if [[ "$state" != "OPEN" ]]; then
+    echo "  skipping closed issue #$issue_number ($field=$value)"
+    return 0
+  fi
+
+  gh project item-edit "$PROJECT_NUMBER" \
+    --owner "$OWNER" \
+    --url "https://github.com/$FULL_REPO/issues/$issue_number" \
+    --field "$field" \
+    --value "$value" >/dev/null
+}
+
+set_many() {
+  local field="$1"
+  local value="$2"
+  shift 2
+  local n
+  for n in "$@"; do
+    set_issue_field "$n" "$field" "$value"
+  done
+}
+
+echo "Assigning priorities..."
+set_many "Priority" "High"   "${PRIORITY_HIGH[@]}"
+set_many "Priority" "Medium" "${PRIORITY_MEDIUM[@]}"
+set_many "Priority" "Low"    "${PRIORITY_LOW[@]}"
+
+echo "Assigning areas..."
+set_many "Area" "Security"       "${AREA_SECURITY[@]}"
+set_many "Area" "Data"           "${AREA_DATA[@]}"
+set_many "Area" "Backend"        "${AREA_BACKEND[@]}"
+set_many "Area" "Frontend"       "${AREA_FRONTEND[@]}"
+set_many "Area" "Infrastructure" "${AREA_INFRASTRUCTURE[@]}"
+if ((${#AREA_DOCUMENTATION[@]})); then
+  set_many "Area" "Documentation" "${AREA_DOCUMENTATION[@]}"
+fi
+
+# If issue #45 still has its implementation PR (#47) open, reflect that in the board.
+if [[ "$(gh issue view 45 -R "$FULL_REPO" --json state --jq '.state')" == "OPEN" ]] && \
+   [[ "$(gh pr view 47 -R "$FULL_REPO" --json state --jq '.state' 2>/dev/null || true)" == "OPEN" ]]; then
+  echo "Marking issue #45 In review..."
+  set_issue_field 45 "Status" "In review"
+fi
+
+echo "Configuring the default table view..."
+TITLE_FIELD_ID="$(
+  gh project field-list "$PROJECT_NUMBER" --owner "$OWNER" --format json \
+    --jq '.fields[] | select(.name=="Title") | .id'
+)"
+PRIORITY_FIELD_ID="$(
+  gh project field-list "$PROJECT_NUMBER" --owner "$OWNER" --format json \
+    --jq '.fields[] | select(.name=="Priority") | .id'
+)"
+AREA_FIELD_ID="$(
+  gh project field-list "$PROJECT_NUMBER" --owner "$OWNER" --format json \
+    --jq '.fields[] | select(.name=="Area") | .id'
+)"
+
+BACKLOG_VIEW_ID="$(
+  gh api graphql \
+    -f query='query($login: String!, $number: Int!) {
+      user(login: $login) {
+        projectV2(number: $number) {
+          views(first: 20) { nodes { id name layout } }
+        }
+      }
+    }' \
+    -f login="$OWNER" \
+    -F number="$PROJECT_NUMBER" \
+    --jq '.data.user.projectV2.views.nodes[] | select(.layout=="TABLE_LAYOUT") | .id' |
+    head -n 1
+)"
+
+if [[ -z "$BACKLOG_VIEW_ID" ]]; then
+  echo "error: could not locate the default table view" >&2
+  exit 1
+fi
+
+gh api graphql -f query="
+mutation {
+  updateProjectV2View(input: {
+    viewId: \"$BACKLOG_VIEW_ID\"
+    name: \"Backlog\"
+    configuration: {
+      visibleFieldIds: [
+        \"$TITLE_FIELD_ID\"
+        \"$STATUS_FIELD_ID\"
+        \"$PRIORITY_FIELD_ID\"
+        \"$AREA_FIELD_ID\"
+      ]
+    }
+  }) {
+    projectV2View { id name layout }
+  }
+}" >/dev/null
+
+echo "Creating Workflow board..."
+TITLE_REST_ID="$(
+  gh api -H "X-GitHub-Api-Version: $API_VERSION" \
+    "/users/$OWNER/projectsV2/$PROJECT_NUMBER/fields" \
+    --jq '.[] | select(.name=="Title") | .id'
+)"
+STATUS_REST_ID="$(
+  gh api -H "X-GitHub-Api-Version: $API_VERSION" \
+    "/users/$OWNER/projectsV2/$PROJECT_NUMBER/fields" \
+    --jq '.[] | select(.name=="Status") | .id'
+)"
+PRIORITY_REST_ID="$(
+  gh api -H "X-GitHub-Api-Version: $API_VERSION" \
+    "/users/$OWNER/projectsV2/$PROJECT_NUMBER/fields" \
+    --jq '.[] | select(.name=="Priority") | .id'
+)"
+AREA_REST_ID="$(
+  gh api -H "X-GitHub-Api-Version: $API_VERSION" \
+    "/users/$OWNER/projectsV2/$PROJECT_NUMBER/fields" \
+    --jq '.[] | select(.name=="Area") | .id'
+)"
+
+WORKFLOW_URL="$(
+  gh api -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: $API_VERSION" \
+    "/users/$OWNER/projectsV2/$PROJECT_NUMBER/views" \
+    -f name="Workflow" \
+    -f layout="board" \
+    -f filter="is:open" \
+    -F "visible_fields[]=$TITLE_REST_ID" \
+    -F "visible_fields[]=$PRIORITY_REST_ID" \
+    -F "visible_fields[]=$AREA_REST_ID" \
+    -F "vertical_group_by[]=$STATUS_REST_ID" \
+    --jq '.html_url'
+)"
+
+echo
+echo "Project setup complete."
+echo "Project:  $PROJECT_URL"
+echo "Workflow: $WORKFLOW_URL"
+echo
+echo "One manual step remains:"
+echo "  Project -> ... -> Workflows -> Auto-add to project -> Edit"
+echo "  Repository: $FULL_REPO"
+echo "  Filter:     is:issue is:open"
+echo "  Then choose: Save and turn on workflow"
+echo
+echo "Current project items:"
+gh project item-list "$PROJECT_NUMBER" --owner "$OWNER" \
+  --limit 200 \
+  --field Status --field Priority --field Area
